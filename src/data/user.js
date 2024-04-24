35dd429208524988ac66a68d307e7d302d4cbefc
Rafael Reyes <rcreyes.951@gmail.com>
2024-04-24 18:46:48 +0000

    changed project name and label for link

--- src/data/user.js
+++ src/data/user.js
@@ -35,47 +35,38 @@ const INFO = {
 
 	projects: [
 		{
-			title: "Project 1",
+			title: "Pre-Energization",
 			description:
 				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
+			linkText: "Learn More",
 			link: "https://github.com",
 		},
 
 		{
-			title: "Project 2",
+			title: "Post-Energization",
 			description:
 				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
-			linkText: "View Project",
+			linkText: "Learn More",
 			link: "https://github.com",
 		},
 
 		{
-			title: "Project 3",
+			title: "Fleet Electrification Planning",
 			description:
 				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
-			linkText: "View Project",
+			linkText: "Learn More",
 			link: "https://github.com",
 		},
 
 		{
-			title: "Project 4",
+			title: "Emerging Technology",
 			description:
 				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
-
-		{
-			title: "Project 5",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
+			linkText: "Learn More",
 			link: "https://github.com",
 		},
 	],
