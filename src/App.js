e8ae2c7877cba6de9090bcb30a9696b4cb3c1cb6
Rafael Reyes <rcreyes.951@gmail.com>
2024-04-24 20:36:24 +0000

    services change

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Homepage />} />
 				<Route path="/about" element={<About />} />
-				<Route path="/projects" element={<Projects />} />
+				<Route path="/services" element={<Projects />} />
 				<Route path="/articles" element={<Articles />} />
 				<Route path="/article/:slug" element={<ReadArticle />} />
 				<Route path="/contact" element={<Contact />} />
